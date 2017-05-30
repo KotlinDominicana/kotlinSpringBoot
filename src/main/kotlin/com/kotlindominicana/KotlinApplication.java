807c574e46851f0cdf97164d2202bdb307e2dfe5
Frangel Hernandez <fhernandez@avathartech.com>
2017-05-30 00:21:06 -0400

    Refactoring the main to java to avoid runnin kotlin app instead of springboot project

---
+++ src/main/kotlin/com/kotlindominicana/KotlinApplication.java
@@ -0,0 +1,11 @@
+package com.kotlindominicana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinApplication {
+    public static void  main(String[] args) {
+        SpringApplication.run(KotlinApplication.class, args);
+    }
+}
